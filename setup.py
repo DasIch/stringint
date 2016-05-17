ec264b1cca2bd1ffff5c25f15d40d8a5dfeab96b
Daniel Neuhäuser <ich@danielneuhaeuser.de>
2016-05-17 02:06:32 +0200

    add description to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
+import os
+
 from setuptools import setup
+
+
+def read_readme():
+    readme_path = os.path.join(os.path.dirname(__file__), 'README.rst')
+    with open(readme_path, 'r', encoding='utf-8') as readme_file:
+        return readme_file.read()
+
 
 setup(
     name='stringint',
@@ -6,6 +15,8 @@ setup(
     url='https://github.com/DasIch/stringint/',
     author='Daniel Neuhäuser',
     author_email='ich@danielneuhaeuser.de',
+    description='represent strings as ints and vice versa',
+    long_description=read_readme(),
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3.5',
